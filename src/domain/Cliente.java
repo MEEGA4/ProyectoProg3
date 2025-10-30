2098bb7dcfd6e38e3be106ddcd6e5b14428a7114
Mikel Oyarzabal Riaño <mikel.oyarzabal@opendeusto.es>
2025-10-30 15:46:52 +0100

    modificado ventanaInicio

--- src/domain/Cliente.java
+++ src/domain/Cliente.java
@@ -1,15 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente extends Persona {
 	private String telefono;
 	private String email;
-	
+	List<Producto> lVistos;
+	List<Producto> lFavoritos;
 	
 	public Cliente(String nombre, String apellido, int edad, String contrasena, String ubicacion, String telefono, String email) {
 		super(nombre, apellido, edad, contrasena, ubicacion);
 		this.telefono = telefono;
 		this.email = email;
+		this.lVistos = new ArrayList<>();
+		this.lFavoritos = new ArrayList<>();
 	}
 
 	public String getTelefono() {
@@ -28,10 +33,28 @@ public class Cliente extends Persona {
 		this.email = email;
 	}
 
-	@Override
-	public String toString() {
-		return "Cliente [telefono=" + telefono + ", email=" + email + ", nombre=" + nombre + ", apellido=" + apellido
-				+ ", edad=" + edad + ", contrasena=" + contrasena + ", ubicacion=" + ubicacion + "]";
+	public List<Producto> getlVistos() {
+		return lVistos;
 	}
 
+	public void setlVistos(List<Producto> lVistos) {
+		this.lVistos = lVistos;
+	}
+
+	public List<Producto> getlFavoritos() {
+		return lFavoritos;
+	}
+
+	public void setlFavoritos(List<Producto> lFavoritos) {
+		this.lFavoritos = lFavoritos;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [telefono=" + telefono + ", email=" + email + ", lVistos=" + lVistos + ", lFavoritos="
+				+ lFavoritos + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", contrasena="
+				+ contrasena + ", ubicacion=" + ubicacion + "]";
+	}
+
+	
 }
